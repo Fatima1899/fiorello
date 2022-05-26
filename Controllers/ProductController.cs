6d82b1add5881df739e9940e7a75ee5f209eb9e7
Fatima1899 <99797429+Fatima1899@users.noreply.github.com>
2022-05-25 21:07:49 -0700

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using fiorello.DAL;
+using fiorello.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace fiorello.Controllers
+{
+    public class ProductController : Controller
+    {
+        private AppDbContext _context;
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Product>products = _context.products.Include(p => p.Category).OrderByDescending(p=>p.Id).Take(8).ToList();
+            return View(products);
+        }
+        public IActionResult LoadMore(int skip)
+        {
+            List<Product> products = _context.products.Include(p => p.Category).Skip(skip).Take(8).ToList();
+            return Json(new{
+                name="kamal"
+            });
+        }
+    }
+}
---
+++ DAL/Migrations/20220525163257_pageIntroDb.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace fiorello.Migrations
+{
+    public partial class pageIntroDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Sliders",
+                table: "Sliders");
+
+            migrationBuilder.RenameTable(
+                name: "Sliders",
+                newName: "sliders");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_sliders",
+                table: "sliders",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "pageIntros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Desc = table.Column<string>(nullable: true),
+                    ImageUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_pageIntros", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "pageIntros");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_sliders",
+                table: "sliders");
+
+            migrationBuilder.RenameTable(
+                name: "sliders",
+                newName: "Sliders");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Sliders",
+                table: "Sliders",
+                column: "Id");
+        }
+    }
+}
